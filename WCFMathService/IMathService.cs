01ecf609dabd125999a29d6f9cfe145edb323b4d
hunghly <hunghly@gmail.com>
2022-11-11 03:40:35 -0600

    revised code to make math api

--- WCFMathService/IMathService.cs
+++ WCFMathService/IMathService.cs
@@ -10,38 +10,33 @@ namespace WCFMathService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IMathService
     {
 
-        [OperationContract]
-        string GetData(int value);
-
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
         // TODO: Add your service operations here
+        [OperationContract]
+        double piValue();
+
+        [OperationContract]
+        int absVAlue(int x);
+
+        [OperationContract]
+        int add2(int x, int y);
+
+        [OperationContract]
+        results computeStat(string str);
     }
 
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
     [DataContract]
-    public class CompositeType
+    public class results
     {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
         [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
+        public int digits { get; set; }
         [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
+        public int upper_case_letters { get; set; }
+        [DataMember]
+        public int lower_case_letters { get; set; }
     }
 }
--- WCFMathService/MathService.svc.cs
+++ WCFMathService/MathService.svc.cs
@@ -10,24 +10,49 @@ namespace WCFMathService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    public class Service1 : IMathService
     {
-        public string GetData(int value)
+        public double piValue()
         {
-            return string.Format("You entered: {0}", value);
+            return Math.PI;
+        }
+        public int absVAlue(int x)
+        {
+            if (x >= 0)
+                return x;
+            else
+                return (-x);
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public int add2(int x, int y)
         {
-            if (composite == null)
+            return x + y;
+        }
+
+        public results computeStat(string str)
+        {
+            results res = new results();
+            int d = 0, u = 0, l = 0;
+            for (int i = 0; i < str.Length; i++)
             {
-                throw new ArgumentNullException("composite");
+                if (Char.IsUpper(str[i]))
+                {
+                    u = u + 1;
+                }
+                if (Char.IsLower(str[i]))
+                {
+                    l = l + 1;
+                }
+                if (Char.IsDigit(str[i]))
+                {
+                    d = d + 1;
+                }
             }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            res.digits = d;
+            res.upper_case_letters = u;
+            res.lower_case_letters = l;
+
+            return res;
         }
     }
 }
